64515579a264e055a8e232a74b701f2b1ca75585
FLAq <flaqmastahgf@gmail.com>
2011-09-13 11:10:55 +0200

    poczatek

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Remont::Application.config.secret_token = '36ab2da2ac7786b21c1be94d4e729060a6ad2a3a6892066068a0143fe30dcea95ea336b1c40b6c55221763120cfac56de1c4debb487bd954f1dfe248950f5a79'

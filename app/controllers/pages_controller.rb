322e7079c681ca0af56a1751f5807841b23e8436
FLAq <flaqmastahgf@gmail.com>
2011-09-13 13:03:01 +0200

    jaja

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+﻿class PagesController < ApplicationController
+  def home
+  @title = "Główna"
+  end
+
+  def contact
+  @title = "Kontakt"
+  end
+  
+  def about
+  @title = "O nas"
+  end
+
+end
